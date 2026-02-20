048561dd2086fae97458650548ce89be7152d2d3
Mostafa_Tarek <mostafatarek6227@gmail.com>
2026-02-20 17:40:36 +0200

    Delete DB.connect.js

--- DB.connect.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-//connect the Database
-const db_connect =()=>{ mongoose.connect('mongodb://localhost:27017/facebook').then(
-    (res)=>
-    {
-        console.log("database connected")
-    }
-)}
-export default db_connect
